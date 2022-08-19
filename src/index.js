c083ebd634ee6ac2d97699793b79d7e27f1476ea
AbdulRehman <likeam99@gmail.com>
2022-08-19 22:27:51 +0500

    Building gameReducer

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { configureStore } from '@reduxjs/toolkit';
+import { legacy_createStore as createStore } from 'redux';
+const store = createStore();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
