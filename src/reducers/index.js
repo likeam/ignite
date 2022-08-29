76fa649ef9cdb4a90aced54361e12486cf7098d5
AbdulRehman <likeam99@gmail.com>
2022-08-29 22:50:16 +0500

    buiding user reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,18 @@
 import { combineReducers } from "redux";
 import gameReducer from "./gamesReducrs";
 
+const initState = {
+    name: "",
+    isLoged: false,
+}
+
+const userReducer = (state = initState, action) => {
+    switch (action.type){
+        default: 
+        return {...state};
+    }
+};
+
 const rootReducer = combineReducers({
     games: gameReducer,
 })
